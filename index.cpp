d5f055cc9bc050ab6c6c0cd4652c2ac824bfac6e
Angel Q <77522699+migurd@users.noreply.github.com>
2022-07-18 15:30:46 -0600

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+struct entrenador {
+	id[10];
+	especialidad[15];
+	primerNombre[15];
+	segundoNombre[15];
+	apellidoMaterno[15];
+	apellidoPaterno[15];
+	telefono[10];
+	correo[20];
+	direccion[20];
+	turno[10];
+} ent[10]; // Usaremos las 3 primeras letras para todas las estructuras
+
+struct servicios {
+	id[10];
+	servicio[20];
+	idInstructor[10];
+	rutina[20];
+} ser[10];
+
+struct registro {
+	id[10];
+	fechaRegistro[11];
+	vigenciaRegistro[11];
+} reg[10];
+
+struct cliente {
+	id[10];
+	primerNombre[15];
+	segundoNombre[15];
+	apellidoMaterno[15];
+	apellidoPaterno[15];
+	telefono[10];
+	correo[20];
+	direccion[20];
+	turno[10];
+	idRegistro[10];
+	idServicios[10];
+} cli[10];
+
+
+
+int main() {
+	
+}
